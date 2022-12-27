0de08530f93a1171a1f2cc5f734dd5c9899419a5
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-12-27 23:45:21 +0300

    tasktracker project done

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,23 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import (
+    todo_list_create,
+    todo_home,
+    todo_detail,
+    Todos,
+    TodoDetail,
+    TodoMVS,
+    )
 
-from django.urls import path
-
+router = routers.DefaultRouter()
+router.register('todo', TodoMVS)
 
 urlpatterns = [
-    path('', ),
-    
+    path('', todo_home),
+    # path('list-create/', todo_list_create),
+    # path('detail/<int:pk>', todo_detail),
+    path('list-create/', Todos.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('', include(router.urls)),
+
 ]
